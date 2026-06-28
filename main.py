a150e2cb57fba0b9630348ae06843aec74b31627
Arturo AH <mrarturoah@gmail.com>
2026-06-28 10:37:09 +1000

    Improving variable names

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ sys.path.insert(0, str(Path(__file__).resolve().parent / "src"))
 from domains.transfers.application.process_transfers import ProcessTransfers
 
 if __name__ == "__main__":
-    account_balances_file_path = "specs/mable_account_balances.csv"
-    transfers_file_path = "specs/mable_transactions.csv"
+    source_account_balances_file_path = "specs/mable_account_balances.csv"
+    source_transfers_file_path = "specs/mable_transactions.csv"
+    reports_file_path = "storage/reports/"
     print(f"Processing transfers:")
-    print(f"-->Source balance files {account_balances_file_path}")
-    print(f"-->Source Transfer file: {transfers_file_path}")
+    print(f"-->Source balance files {source_account_balances_file_path}")
+    print(f"-->Source Transfer file: {source_transfers_file_path}")
     print("--------------")
     result = ProcessTransfers(
-        account_balances_file_path,
-        transfers_file_path,
+        source_account_balances_file_path,
+        source_transfers_file_path,
+        reports_file_path
     ).run()
 
     print(f"Updated balances written to {result.balances_path}")
